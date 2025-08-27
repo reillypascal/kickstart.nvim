4aa8cd74ac9c302d191bc4b9dc7228a53a83c42b
Reilly Spitzfaden <reillypascal@gmail.com>
2025-08-27 13:45:43 -0400

    prettierd for markdown

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,7 @@
 -- https://www.reddit.com/r/neovim/comments/ymz2r8/problem_with_line_wrap_in_markdown_files_in_lua/
 -- equivalent to `:set linebreak` or `:set lbr`
-vim.opt.linebreak = true
+--vim.opt.linebreak = true
+vim.opt.textwidth = 95
+-- https://neovim.io/doc/user/options.html#'conceallevel'
+-- hides links, but also adds weird line breaks, hides image links, etc.
+-- vim.opt.conceallevel = 2
