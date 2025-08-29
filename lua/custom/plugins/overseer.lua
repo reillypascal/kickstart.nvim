3340c25949dcb63aabe7c9386429e159e9f82fd6
Reilly Spitzfaden <reillypascal@gmail.com>
2025-08-28 23:04:14 -0400

    use toggleterm for nvim-dap tasks in *overseer*

--- lua/custom/plugins/overseer.lua
+++ lua/custom/plugins/overseer.lua
@@ -2,6 +2,9 @@ return {
   'stevearc/overseer.nvim',
   opts = {},
   config = function()
-    require('overseer').setup()
+    require('overseer').setup {
+      -- https://github.com/stevearc/overseer.nvim/blob/master/doc/third_party.md#toggleterm
+      strategy = 'toggleterm',
+    }
   end,
 }
