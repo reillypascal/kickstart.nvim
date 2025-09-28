35dccc5f5f50072daec230fccbf523674bc4205c
Reilly Spitzfaden <reillypascal@gmail.com>
2025-09-28 12:15:55 -0400

    added :IsoDate user command

--- init.lua
+++ init.lua
@@ -220,6 +220,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- [[ User commands ]]
+vim.api.nvim_create_user_command('IsoDate', "pu=strftime('%Y-%m-%dT%H:%M:%S%z')", {})
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
